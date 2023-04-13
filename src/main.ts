d537fb46d76769995f03f015189c99cb56be80c0
asagafonov <a.s.agafonov@yandex.ru>
2023-04-13 17:45:40 +0300

    remove i18n

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,13 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
-import { createI18n } from 'vue-i18n';
-
-import translations from '../public/locales/translations';
 
 import App from './App.vue';
 import router from './router';
 
 import './assets/main.css';
 
-const i18n = createI18n({
-  allowComposition: true,
-  locale: 'en',
-  messages: translations,
-});
-
 const app = createApp(App);
 
-app.use(i18n);
 app.use(createPinia());
 app.use(router);
 
